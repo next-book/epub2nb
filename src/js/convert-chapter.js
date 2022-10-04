7b3f717596a5e2e17d41ec33f56a206155d25762
Jan Martinek <honza.martinek@gmail.com>
2022-10-04 17:44:35 +0200

    Fix italics removal from footnotes

--- src/js/convert-chapter.js
+++ src/js/convert-chapter.js
@@ -185,7 +185,7 @@ const replaceFootnotes = mdText => {
   return (
     mdText
       // remove italics around footnote body
-      .replace(/\n_(\[(\d+)\]\([^\)]*#footnote-[^\)]+?-backlink\).+)_\n/g, '$1')
+      .replace(/\n_(\[(\d+)\]\([^\)]*#footnote-[^\)]+?-backlink\).+)_\n/g, '\n$1\n')
       // [94](<self-link>#footnote-19288-94-backlink)
       .replace(/\n\[(\d+)\]\([^\)]*#footnote-[^\)]+?-backlink\) ?/g, '\n[^$1]: ')
       // [94](<self-link>#footnote-19288-94)
